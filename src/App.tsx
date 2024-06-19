1967914a1159ba0d79baaf5bb74ea2c38cc1a7de
sjeevankumar <sjeevankumar89@gmail.com>
2024-06-19 14:33:21 +0530

    feat:add header ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import {lazy,Suspense} from "react"
 import Loader from './components/loader'
+import Header from './components/header'
 
 const Home = lazy(()=>import("./pages/home"))
 const Search = lazy(()=>import("./pages/search"))
@@ -29,6 +30,8 @@ const App = () => {
   return (
     <Router>
       {/* Header */}
+      <Header/>
+      
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Home />} />
